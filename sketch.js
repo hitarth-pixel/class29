70176930630ecd07ba1c066037b7ca10c9d31e5e
hitarth-pixel <75882978+hitarth-pixel@users.noreply.github.com>
2021-01-17 17:07:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -78,3 +78,11 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
+function keyPressed(){
+    if(keyCode===32){
+
+       Matter.Body.setPosition(bird.body,{x:160,y:100});
+
+    }
+}
